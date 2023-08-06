f180dec5462835db7d340435a01a0fce5c6cf751
Brendan Early <mymindstorm@evermiss.net>
2023-08-06 17:39:20 -0500

    lint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <div id="container">
-    <h1>Brendan's Website</h1>
-    <p>WIP</p>
+	<h1>Brendan's Website</h1>
+	<p>WIP</p>
 </div>
+
 <style>
-#container {
-    margin-left: 1.5vw;
-    margin-right: 1.5vw;
-}
+	#container {
+		margin-left: 1.5vw;
+		margin-right: 1.5vw;
+	}
 </style>
--- src/routes/graphing/+page.svelte
+++ src/routes/graphing/+page.svelte
@@ -1,74 +1,77 @@
-<div id="container">
-    <h1>Three.js Test</h1>
-    <canvas id="three-canvas" bind:this={canvas} />
-</div>
-
 <script lang="ts">
-    import * as THREE from 'three';
-    import { onMount } from 'svelte';
+	import * as THREE from 'three';
+	import { onMount } from 'svelte';
 
-    let canvas: HTMLCanvasElement;
-    let innerHeight: number;
+	let canvas: HTMLCanvasElement;
 
-    onMount(() => {
-        const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+	onMount(() => {
+		const scene = new THREE.Scene();
+		const camera = new THREE.PerspectiveCamera(
+			75,
+			window.innerWidth / window.innerHeight,
+			0.1,
+			1000
+		);
 
-        const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
-        // renderer.setClearColor(0xffffff)
+		const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+		// renderer.setClearColor(0xffffff)
 
+		const geometry = new THREE.BoxGeometry(1, 1, 1);
+		const material = new THREE.MeshNormalMaterial();
+		const cube = new THREE.Mesh(geometry, material);
+		scene.add(cube);
 
-        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        const material = new THREE.MeshNormalMaterial();
-        const cube = new THREE.Mesh( geometry, material );
-        scene.add( cube );
+		camera.position.z = 5;
 
-        camera.position.z = 5;
+		function resizeCanvasToDisplaySize() {
+			const canvas = renderer.domElement;
+			// look up the size the canvas is being displayed
+			const width = canvas.clientWidth;
+			const height = canvas.clientHeight;
 
-        function resizeCanvasToDisplaySize() {
-            const canvas = renderer.domElement;
-            // look up the size the canvas is being displayed
-            const width = canvas.clientWidth;
-            const height = canvas.clientHeight;
+			// adjust displayBuffer size to match
+			if (canvas.width !== width || canvas.height !== height) {
+				// you must pass false here or three.js sadly fights the browser
+				renderer.setSize(width, height, false);
+				camera.aspect = width / height;
+				camera.updateProjectionMatrix();
 
-            // adjust displayBuffer size to match
-            if (canvas.width !== width || canvas.height !== height) {
-                // you must pass false here or three.js sadly fights the browser
-                renderer.setSize(width, height, false);
-                camera.aspect = width / height;
-                camera.updateProjectionMatrix();
+				// update any render target sizes here
+			}
+		}
 
-                // update any render target sizes here
-            }
-        }
+		function animate() {
+			requestAnimationFrame(animate);
 
-        function animate() {
-            requestAnimationFrame( animate );
+			resizeCanvasToDisplaySize();
 
-            resizeCanvasToDisplaySize()
+			cube.rotation.x += 0.01;
+			cube.rotation.y += 0.01;
 
-            cube.rotation.x += 0.01;
-	        cube.rotation.y += 0.01;
-
-            renderer.render( scene, camera );
-        }
-        animate();
-    })
+			renderer.render(scene, camera);
+		}
+		animate();
+	});
 </script>
 
+<div id="container">
+	<h1>Three.js Test</h1>
+	<canvas id="three-canvas" bind:this={canvas} />
+</div>
+
 <style>
-h1 {
-    margin-left: 1.5vw;
-}
+	h1 {
+		margin-left: 1.5vw;
+	}
 
-#three-canvas {
-    height: 100%;
-    width: 100%;
-}
+	#three-canvas {
+		height: 100%;
+		width: 100%;
+	}
 
-#container {
-    display: flex;
-    flex-flow: column;
-    height: 100%;
-}
+	#container {
+		display: flex;
+		flex-flow: column;
+		height: 100%;
+	}
 </style>
